138df685e276d43043189ba1eac37f4ea3b32315
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-04-18 22:08:20 +0300

    fix(exceptions): removed NotUniqueItemException from MathBox class and refactored this class to use it.

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -1,5 +1,5 @@
 public class Exceptions{
-    class NotUniqueItemException extends Exception{
+    static class NotUniqueItemException extends Exception{
         NotUniqueItemException(String message){
             super(message);
         }
