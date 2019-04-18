f5c321528657a17316589e07f67d3e7fd4e89942
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-04-18 22:11:14 +0300

    feat(add): added feature add to ObjectBox to add an object to this box

--- src/ObjectBox.java
+++ src/ObjectBox.java
@@ -6,4 +6,12 @@ public class ObjectBox{
     public ObjectBox(){
         data = new ArrayList<>();
     }
+
+    public void add(Object o) throws Exceptions.NotUniqueItemException{
+        if(data.contains(o)){
+            throw new Exceptions.NotUniqueItemException("Object " + o.toString() + " is a duplicate");
+        }
+
+        data.add(o);
+    }
 }
